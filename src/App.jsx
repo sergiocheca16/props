09af9c5afdc0f795796c0f92090a95059033c9c1
Carlos <carlosdiazgirol@hotmail.com>
2024-03-21 15:18:36 +0100

    ready to work

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'Hacer la compra', completed: false },
+    { id: 2, text: 'Llamar al médico', completed: true },
+    { id: 3, text: 'Hacer ejercicio', completed: false }
+  ]);
+
+  
+
+  return (
+    <></>
+  );
+};
+
+export default App;
